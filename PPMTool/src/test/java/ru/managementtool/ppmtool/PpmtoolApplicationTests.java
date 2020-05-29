2b45d2025b4c227fd8ce17c0ee117ec0653095ea
anaumov <anaumov@cma.ru>
2020-05-29 21:59:01 +0300

    TODO add tests

--- PPMTool/src/test/java/ru/managementtool/ppmtool/PpmtoolApplicationTests.java
+++ PPMTool/src/test/java/ru/managementtool/ppmtool/PpmtoolApplicationTests.java
@@ -1,13 +1,10 @@
 package ru.managementtool.ppmtool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PpmtoolApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // TODO add tests
 
 }
